e22f1a0e9a5da3a1df4391574a5ae90e9de2e24d
Fernando E Zimerman <fernando.ezimerman@gmail.com>
2022-06-03 21:33:44 -0300

    Primeira parte com POO

---
+++ src/main/kotlin/POO/ClassesEnum.kt
@@ -0,0 +1,34 @@
+package POO
+
+/*
+ * Classes enum permitem a definição de uma lista de valores dentro da classe.
+ * Útil quando é necessário transitar um intervalo de valores conhecidos,
+ * pois restringe as possibilidades e os erros.
+ */
+fun main() {
+
+    // Por exemplo, uma classe que contenha os meses do ano.
+    // Ou uma classe que tenha os dias da semana
+
+    // Percorre a lista de valores
+    for (p in Prioridade.values()) {
+        println(p)
+    }
+
+    // Valor associado a prioridade
+    println(Prioridade2.ALTA.id)
+
+}
+
+enum class Prioridade {
+    BAIXA {
+        override fun toString(): String {
+            return "Essa prioridade é baixa."
+        }
+    },
+    MEDIA, ALTA
+}
+
+enum class Prioridade2(val id: Int) {
+    BAIXA(1), MEDIA(5), ALTA(10)
+}
