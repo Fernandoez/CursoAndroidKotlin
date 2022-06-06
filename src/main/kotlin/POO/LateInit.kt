532c54ad6af8c0db5c4c8a6be14f3546b1e18b77
Fernando E Zimerman <fernando.ezimerman@gmail.com>
2022-06-06 20:44:36 -0300

    Finalizando Secao de POO

---
+++ src/main/kotlin/POO/LateInit.kt
@@ -0,0 +1,27 @@
+package POO
+
+/*
+ * Em alguns casos, é necessário que haja um processamento ou uma lógica antes da atribuição do valor a variável.
+ * Para isso e também evitar declarar a variável como nula, é usado a palavra lateinit.
+ *
+ * Variáveis declaradas com lateinit não são alocadas na memória até que sejam inicializadas.
+ */
+fun main() {
+    val receita = Receita()
+
+    // Acessar a variável sem antes inicializar resulta em erro.
+    //println(receita.instrucoes)
+
+    //Inicializando
+    receita.geraReceita()
+    println(receita.instrucoes)
+}
+
+class Receita {
+    lateinit var instrucoes: String
+
+    fun geraReceita() {
+        // Preenchimento através da função
+        instrucoes = "Lave as mãos."
+    }
+}
